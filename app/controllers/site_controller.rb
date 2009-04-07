7e98b889406d3688a8bafc6c0eb22b96a88c45cb
Benjamin Meichsner <benni@dresdener27.de>
2009-04-07 19:40:11 +0200

    Bertsch: Improved sidebar. Added some cool new icons, considering the corporate design..

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,23 +1,2 @@
 class SiteController < ApplicationController
-  def index
-  end
-
-  def news
-  end
-
-  def supported
-  end
-  
-  def story
-  end
-
-  def terms
-  end
-
-  def privacy
-  end
-
-  def contact
-  end
-
 end
